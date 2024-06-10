356169b30525b096d1f150dd22f7210b8d6ee6df
Jovan Louis Lemuel <jovanlemuel9103@gmail.com>
2024-06-10 17:55:44 +0700

    update routes and add student-dashboard

--- src/js/server.js
+++ src/js/server.js
@@ -133,6 +133,43 @@ app.get("/teacher-dashboard", async (request, response) => {
   });
 });
 
+// app.get("/student-dashboard", async (request, response) => {
+//   let tx = await RC.getStudent(addr, mataPelajaranId);
+//   console.log(tx);
+
+//   let studentSubjects = await prisma.studentPelajaran.findMany();
+//   let pelajarans = await prisma.mataPelajaran.findMany();
+//   let students = await prisma.student.findMany();
+//   response.render("student-dashboard", {
+//     subjects: pelajarans.length ? pelajarans : [],
+//   });
+// });
+
+app.get("/student-dashboard", async (request, response) => {
+  const addr = request.cookies.addr;
+
+  try {
+    let studentSubjectIds = await RC.getStudentMataPelajarans(addr);
+    console.log("Student Subject IDs: ", studentSubjectIds);
+
+    let subjects = [];
+    if (studentSubjectIds.length > 0) {
+      subjects = await prisma.mataPelajaran.findMany({
+        where: {
+          id: { in: studentSubjectIds.map((id) => id.toString()) },
+        },
+      });
+    }
+
+    response.render("student-dashboard", {
+      subjects: subjects.length ? subjects : [],
+    });
+  } catch (error) {
+    console.error("Error fetching student dashboard data:", error);
+    response.status(500).send("Internal server error");
+  }
+});
+
 app.post("/create-subject", async (request, response) => {
   var name = request.body.subjectName;
   var teacherAddress = request.body.teacherAddress;
@@ -153,64 +190,12 @@ app.post("/create-subject", async (request, response) => {
       .catch((error) => {
         console.error("Error memanggil endpoint:", error);
       });
-    response.redirect("/manageSubject");
+    response.redirect("/teacher-dashboard");
   } catch (err) {
-    response.redirect("/manageSubject");
+    response.redirect("/teacher-dashboard");
     console.log(err);
   }
 });
-
-// app.get("/login", function (request, response) {
-//   var addr = request.body.address;
-//   let tx = RC.checkTeachers(addr);
-//   response.render("pages/login", {});
-// })
-
-// app.post('/auth', async (request, response) => {
-//     var addr = request.body.address;
-//     var pwd = request.body.password;
-
-//     try {
-//         RC.getVoter(addr).then(function(res) {
-//             console.log(res);
-//             if(res == true) {
-//                 response.cookie('addr', addr);
-//                 response.redirect('/voting');
-//             } else {
-//                 response.send();
-//             }
-//         });
-//     } catch(err) {
-//         console.log(err);
-//     }
-// });
-
-// app.get('/voting', async (request, response)=> {
-//     let candidateCounter = await RC.candCounter();
-//     var _addr = request.cookies.addr
-//     var _candObj = [];
-//     for(let i=1; i <= candidateCounter; i++) {
-//         let _candidates = await RC.candidates(i);
-//         _candObj[i-1] = {id:_candidates['id'],name:_candidates['name'],voteCount:_candidates['voteCount']};
-//     }
-//     response.render('pages/voting', {
-//         candList:_candObj,
-//         addr: _addr
-//     });
-// });
-
-// app.post('/vote', async function (request, response) {
-//     console.log(request.body);
-//     var candId = request.body.candSelect;
-//     var addr = request.body.accountAddress;
-//     try {
-//         let tx = await RC.vote(candId, addr);
-//         console.log(tx);
-//         response.redirect('/voting');
-//     } catch(err) {
-//         console.log(err);
-//     }
-// });
 
 app.listen(8000, async (request, response) => {
   console.log("I'm listening on port 8000");
